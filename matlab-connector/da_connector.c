39326ad72476a4bdd09a7b2cec66228612070240
Goutham <gxc2kor@bosch.com>
2026-03-10 05:23:09 +0000

    made into one build script

--- matlab-connector/da_connector.c
+++ matlab-connector/da_connector.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 #ifdef _WIN32
-// Windows-specific includes and dummy implementation
+// Windows-specific stub
 double da_connector()
 {
   fprintf(stderr, "ERROR: da_connector() is not supported on Windows\n");
@@ -12,9 +12,13 @@ double da_connector()
 
 #include <time.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <sys/un.h>
-#include <unistd.h>
+
+/*
+ * The actual implementation of kuksa_get_wiper_mode() lives in client.cpp
+ * (GRC client implementation). We only declare it here so da_connector
+ * can call it.
+ */
+extern const char *kuksa_get_wiper_mode(void);
 
 // Static buffer to track the last logged message
 static char last_message[256] = "";
@@ -55,70 +59,6 @@ void log_message(const char *message)
   last_message[sizeof(last_message) - 1] = '\0';
 }
 
-int uds_listen(char *buffer, size_t buffer_size)
-{
-  int sock_fd;
-  struct sockaddr_un addr;
-  ssize_t bytes_received;
-  const char *socket_path = "/tmp/wiper_status.sock";
-
-  sock_fd = socket(AF_UNIX, SOCK_STREAM, 0);
-  if (sock_fd == -1)
-  {
-    fprintf(stderr, "Failed to create socket file descriptor\n");
-    return -1;
-  }
-
-  memset(&addr, 0, sizeof(addr));
-  addr.sun_family = AF_UNIX;
-  strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path) - 1);
-
-  if (connect(sock_fd, (struct sockaddr *)&addr, sizeof(addr)) == -1)
-  {
-    fprintf(stderr, "Failed to connect to socket: %s\n", socket_path);
-    close(sock_fd);
-    return -1;
-  }
-
-  bytes_received = recv(sock_fd, buffer, buffer_size - 1, 0);
-  if (bytes_received > 0)
-  {
-    buffer[bytes_received] = '\0';
-    if (buffer[bytes_received - 1] == '\n')
-    {
-      buffer[bytes_received - 1] = '\0';
-    }
-  }
-  else if (bytes_received == 0)
-  {
-    close(sock_fd);
-    return 0;
-  }
-  else
-  {
-    close(sock_fd);
-    return -1;
-  }
-
-  close(sock_fd);
-  return bytes_received;
-}
-
-const char *kuksa_get_wiper_mode(void)
-{
-  static char mode[64] = "OFF";
-  char buffer[128] = "";
-
-  int result = uds_listen(buffer, sizeof(buffer));
-  if (result > 0)
-  {
-    strncpy(mode, buffer, sizeof(mode) - 1);
-    mode[sizeof(mode) - 1] = '\0';
-  }
-
-  return mode;
-}
-
 double da_connector()
 {
   const char *mode = kuksa_get_wiper_mode();
@@ -146,9 +86,17 @@ double da_connector()
   {
     return 1000;
   }
+  else if (strcmp(mode, "INTERVAL") == 0)
+  {
+    return 1250;
+  }
+  else if (strcmp(mode, "RAIN_SENSOR") == 0)
+  {
+    return 1500;
+  }
 
   // Default return if no valid mode received
   return 0;
 }
 
-#endif // win
+#endif // _WIN32
